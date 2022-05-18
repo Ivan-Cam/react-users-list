e03c0788f3438f62886037e68607445e918f1be8
Ivan-Campos <ivan820plus@gmail.com>
2022-05-18 17:00:53 -0500

    components and props

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const app = <img src='/logo.svg'></img>;
+const Title = () => <h1>Listado de usuario</h1>;
+
+const User = ({ name, active, role }) => {
+	return (
+		<div className='user'>
+			<span className='name'>{name}</span>
+			<span className='active'>{active}</span>
+			<span className='role'>{role}</span>
+		</div>
+	);
+};
+
+const app = (
+	<div className='list'>
+		<Title />
+		<User name='Pablo Castellano' active='Activo' role='Profesor' />
+		<User name='Jose Ivan Campos Chavez' active='Activo' role='Profesor' />
+	</div>
+);
+
 const container = document.getElementById('root');
 
 ReactDOM.createRoot(container).render(app);
